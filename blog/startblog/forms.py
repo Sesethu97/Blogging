1ad976fea416770bb77ce8e57326fa464058d04e
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-08-10 08:08:05 +0200

    add blog categories stage 1

--- blog/startblog/forms.py
+++ blog/startblog/forms.py
@@ -1,5 +1,15 @@
+from secrets import choice
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+ 
+choices =[("uncategorized", "uncategorized"),("sports", "sports"),("food&drinks", "food&drinks"),("entertainment", "entertainment"),("fashion", "fashion"),("photography&arts", "photography&arts"),("travels", "travels"),]
+# choices = Category.objects.all().values_list("name", "name")
+
+choices_list = []
+
+for item in choices:
+    choices_list.append(item)
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
@@ -8,13 +18,16 @@ class CreatePostForm(forms.ModelForm):
             "title",
             "title_tag",
             "author",
+            "category",
             "body",
         )
         widgets = {
             "title": forms.TextInput(attrs={"class":"form-control"}),
             "title_tag": forms.TextInput(attrs={"class":"form-control"}),
             "author": forms.Select(attrs={"class":"form-control"}),
-            "body": forms.Textarea(attrs={"class":"form-control"})
+            "category": forms.Select(choices=choices_list, attrs={"class":"form-control"}),
+
+            "body": forms.Textarea(attrs={"class":"form-control"}),
 
             }
 
--- blog/startblog/models.py
+++ blog/startblog/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category, name
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -5,12 +6,24 @@ from datetime import datetime, date
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    
+    def get_absolute_url(self):
+        return reverse("blog:home")
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="my blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default="uncategorized")
 
     def __str__(self):
         return self.title + " | " + str(self.author)
