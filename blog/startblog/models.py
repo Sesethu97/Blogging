02906ec13ddfa4dbaec54787687597a1194281dd
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-09-07 13:13:58 +0200

    updates

--- blog/startblog/models.py
+++ blog/startblog/models.py
@@ -1,5 +1,6 @@
 from contextlib import nullcontext
 from curses.ascii import NUL
+from tkinter import Widget
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
--- blog/users/forms.py
+++ blog/users/forms.py
@@ -62,21 +62,6 @@ class EditProfileForm(UserChangeForm):
     username = forms.CharField(
         max_length=150, widget=forms.TextInput(attrs={"class": "form-control"})
     )
-    # last_login = forms.CharField(
-    #     max_length=150, widget=forms.TextInput(attrs={"class": "form-control"})
-    # )
-    # is_superuser = forms.CharField(
-    #     max_length=150, widget=forms.CheckboxInput(attrs={"class": "form-check"})
-    # )
-    # is_staff = forms.CharField(
-    #     max_length=150, widget=forms.CheckboxInput(attrs={"class": "form-check"})
-    # )
-    # is_active = forms.CharField(
-    #     max_length=150, widget=forms.CheckboxInput(attrs={"class": "form-check"})
-    # )
-    # date_joined = forms.CharField(
-    #     max_length=150, widget=forms.TextInput(attrs={"class": "form-control"})
-    # )
 
     class Meta:
         model = User
@@ -86,10 +71,7 @@ class EditProfileForm(UserChangeForm):
             "last_name",
             "email",
             "password",
-            # "facebook_url",
-            # "twitter_url",
-            # "instagram_url", 
-            # "pinterest_url",
+          
           
         ]
 
